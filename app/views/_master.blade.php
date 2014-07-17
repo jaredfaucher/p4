cdf5c05c0335300564954aa551f797aae1aab0a9
Jared Faucher <jared.faucher@gmail.com>
2014-07-17 11:08:24 -0400

    altered readme. started basic blade structure

---
+++ app/views/_master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">		
+		<title>
+			@yield('title', 'Developers Best Friend')
+		</title>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/p4.css">
+	</head>
+	<body>
+		<div class="container">
+			@yield('content')
+		</div>
+	</body>
+</html>
---
+++ app/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('_master')
+		
+@section('title')
+	Bike Swap
+@stop
+
+@section('content')
+	<h1>Bike Swap</h1>
+	<blockquote>
+		<h4>by Jared Faucher<br>Dynamic Web Applications, Summer 2014</h4>
+		<p>A community where people can try out, trade and buy/sell bike parts with people in their community</p>
+	</blockquote>
+@stop
