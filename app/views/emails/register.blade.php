3448f7e68298e27fa6ea7139bc81a8a792a1de9e
Jared Faucher <jared.faucher@gmail.com>
2014-07-29 11:51:13 -0400

    cleaned up email

--- app/views/emails/register.blade.php
+++ app/views/emails/register.blade.php
@@ -1,5 +1,9 @@
+Hi {{ $username }},
+<br>
 Thank you for registering with Bike Swap!
 <br>
-Your username is {{ $username }}
-<br>
 Click <a href="localhost/login">here</a> to return to Bike Swap and Login!
+<br>
+Thanks,
+<br>
+Bike Swap
--- app/views/emails/request.blade.php
+++ app/views/emails/request.blade.php
@@ -1,11 +1,12 @@
-Hey {{ $toUser->username }}
+Hey {{ $toUser->username }},
 <br>
 You have someone who is interested in one of your bike parts!
-<br>
+<p>
 {{ $fromUser->username }} is interested in your {{ $part->part_name }} {{ $part->type }}.  
 If you would like to contact them about a trade, sale or anything 
 else email them at {{ $fromUser->email }}.  Don't forget to update
 your parts profile if you do decide to make an exchange!
+</p>
 <br>
 Thanks,
 <br>
